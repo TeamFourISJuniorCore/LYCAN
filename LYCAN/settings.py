0ed919d7214a25f263213d710e0cccd8144467f3
Mitch McCoard <mitch.mccoard@gmail.com>
2022-12-12 14:08:48 -0700

    hide password

--- LYCAN/settings.py
+++ LYCAN/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lycan',
         'USER' : 'postgres',
-        'PASSWORD' : 'Gr@ft1ng',
+        'PASSWORD' : getpass(),
         'HOST' : 'localhost'
     }
 }
