324a3b919a7383965db42122c825825e463c2cf0
Trevor Heath <trevormheath@gmail.com>
2022-12-13 10:49:40 -0700

    removed unused code

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import * # indexPageView, contactPageView, flashcardPageView, loginPageView, libraryPageView
+from .views import *
 
 urlpatterns = [
     path("", indexPageView, name="index"),
